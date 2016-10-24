7007700b3f6072d4946bf24106a02559b07c8cda
Frank Rosario <FRosario@gmail.com>
2016-10-24 01:25:01 -0400

    Refactored main command execution loop to be a little cleaner.

--- Huge.DrawingTool/Program.cs
+++ Huge.DrawingTool/Program.cs
@@ -11,9 +11,12 @@ namespace Huge.DrawingTool
     public class Program
     {
 
+        const int DEFAULT_ERROR_EXIT_CODE = -255;
+
         public static void LogError(string errMsg)
         {
             Console.Error.WriteLine(errMsg);
+            Exit();
         }
 
         public static void PrintChar(char c)
@@ -26,13 +29,17 @@ namespace Huge.DrawingTool
             Console.Out.WriteLine(msg);
         }
 
+        public static void Exit(int exitCode = DEFAULT_ERROR_EXIT_CODE)
+        {
+            Console.ReadLine();
+            Environment.Exit(exitCode);
+        }
+
         public static void Main(string[] args)
         {
             if(args.Count() != 2)
             {
                 LogError(@"Error: command line arguments incorrect. Format: Huge.DrawingTool c:\path\to\input.text c:\path\to\output.txt");
-                Console.ReadLine();
-                return;
             }
 
             string fileInputPath = args[0];
@@ -43,10 +50,8 @@ namespace Huge.DrawingTool
             if (System.IO.File.Exists(fileInputPath) == false)
             {
                 LogError(string.Format(@"Error: '{0}' could not be found", fileInputPath));
-                Console.ReadLine();
-                return;
             }
-            
+
 
 
             //process input
@@ -55,25 +60,30 @@ namespace Huge.DrawingTool
             var sanitizedCommandLines = CommandParserHelper.SanitizeInput(commandTextLines).ToList();
             var ctx = new ExecutionContext();
 
+            var commands = sanitizedCommandLines.Select(s => CommandParserHelper.GetCommandFromCommandLine(ctx, s)).ToList();
+
+            if (commands.Any() == false)
+            {
+                LogError(string.Format("Error: no commands to execute in file '{0}'", fileInputPath));
+            }
+
             //  extract commands from input
             //make sure first command is creating Canvas
             IList<string> outputScreens = new List<string>();
-            var firstCmd = CommandParserHelper.GetCommandFromCommandLine(ctx, sanitizedCommandLines.First());
+
+            var firstCmd = commands.FirstOrDefault();
+
             if ((firstCmd is CreateCanvasCommand) == false)
             {
                 LogError("Error: first command must be to create canvas");
                 Console.ReadLine();
             }
-            firstCmd.Execute();
-            var gfxBufferStr = ctx.Canvas.DumpBufferToString();
-            outputScreens.Add(gfxBufferStr);
-            Console.WriteLine(gfxBufferStr);
-
-            var remainderCommands = sanitizedCommandLines.Skip(1).Select(s => CommandParserHelper.GetCommandFromCommandLine(ctx, s)).ToList();
-
+            
             //Write to file
 
-            foreach (var command in remainderCommands)
+            string gfxBufferStr = null;
+
+            foreach (var command in commands)
             {
                 command.Execute();
                 gfxBufferStr = ctx.Canvas.DumpBufferToString();
